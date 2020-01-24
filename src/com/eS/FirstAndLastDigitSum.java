c4f00e5a3a85f983ae702ed59481f2f5ba8027a8
es <StaNC2000>
2020-01-24 09:03:17 +0100

    Initial commit

---
+++ src/com/eS/FirstAndLastDigitSum.java
@@ -0,0 +1,14 @@
+package com.eS;
+
+public class FirstAndLastDigitSum {
+
+    public static int sumFirstAndLastDigit(int number){
+
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
